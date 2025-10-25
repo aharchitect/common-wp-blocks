1810a6b78565cce35b8ca4316865cc8d23689434
aharchitect <andreas@aharchitect.de>
2025-10-25 19:33:57 +0200

    Initial commit of common-wp-blocks block scaffolding

---
+++ common-wp-blocks/build/common-wp-blocks/index.asset.php
@@ -0,0 +1,1 @@
+<?php return array('dependencies' => array('react-jsx-runtime', 'wp-block-editor', 'wp-blocks', 'wp-i18n'), 'version' => '39d713f8bdb0f6b8c921');
---
+++ common-wp-blocks/build/common-wp-blocks/view.asset.php
@@ -0,0 +1,1 @@
+<?php return array('dependencies' => array(), 'version' => '5a9cb45130e6f806d910');
