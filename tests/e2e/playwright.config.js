6b528200605469d96021c91900d57582dd54924f
Andreas Hinkelmann aharchitect <andreas@aharchitect.de>
2025-12-14 13:50:29 +0100

    feat: add playwright and docker images for e2e tests

---
+++ tests/e2e/playwright.config.js
@@ -0,0 +1,13 @@
+const { defineConfig } = require('@playwright/test');
+
+module.exports = defineConfig({
+  testDir: './',
+  timeout: 120_000,
+  expect: {
+    timeout: 5000
+  },
+  use: {
+    headless: true,
+    baseURL: process.env.WP_BASE_URL || 'http://localhost:8000'
+  },
+});
